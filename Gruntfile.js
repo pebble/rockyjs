2205b0bb87a32a2a896ea6f9b24fb8138d87d1e8
Heiko Behrens <mail@HeikoBehrens.net>
2015-12-20 13:37:34 +0100

    grunt-newer for uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,18 +10,22 @@ module.exports = function(grunt) {
                 src: '<%= tintin_root %>/build/applib/applib-targets/emscripten/applib.js',
                 dest: 'src/js/rocky_transpiled.js'
             }
+        },
+        newer: {
+            options: {
+                cache: '.cache'
+            }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
+    grunt.loadNpmTasks('grunt-newer');
 
     var default_tasks = [];
 
     // only run uglify per default if transpiled applib exists at TINTIN_ROOT
     if (glob.sync(grunt.config('uglify').applib.src).length > 0) {
-        default_tasks.push("uglify:applib");
+        default_tasks.push("newer:uglify:applib");
     } else {
         grunt.verbose.write("Cannot find transpiled applib at " + grunt.config('uglify').applib.src + " - skipping uglify")
     }
