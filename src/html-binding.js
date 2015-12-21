a065fb1ad37e5ad353a2a6642ce56f3dff79bfde
Heiko Behrens <mail@HeikoBehrens.net>
2015-12-21 13:38:20 +0100

    scaling when rendering to canvas (instead of using transform:scale(3))

--- src/html-binding.js
+++ src/html-binding.js
@@ -2,16 +2,23 @@ if (typeof(Rocky) == "undefined") {
     Rocky = {};
 }
 
-Rocky.bindCanvas= function(canvas) {
+Rocky.bindCanvas= function(canvas, options) {
+    options = options || {};
+
     var module = this.Module();
 
-    var w = canvas.width;
-    var h = canvas.height;
+    var canvasW = canvas.width;
+    var canvasH = canvas.height;
+    var framebufferW = 144;
+    var framebufferH = 168;
+
+    var scale = options.scale || Math.min(canvasW/framebufferW, canvasH/framebufferH);
+
     var canvasCtx = canvas.getContext("2d");
-    var canvasPixelData = canvasCtx.createImageData(w, h);
+    var canvasPixelData = canvasCtx.createImageData(canvasW, canvasH);
     var canvasPixels = canvasPixelData.data;
     var pebblePixelPTR = module.ccall("emx_graphics_get_pixels", "number", []);
-    var pebblePixels = new Uint8Array(module.HEAPU8.buffer, pebblePixelPTR, w*h);
+    var pebblePixels = new Uint8Array(module.HEAPU8.buffer, pebblePixelPTR, canvasW*canvasH);
     var pebbleCtx = module.ccall("app_state_get_graphics_context", "number", []);
 
     var non_c_binding_keys = [];
@@ -20,7 +27,7 @@ Rocky.bindCanvas= function(canvas) {
         update_proc: function(ctx, bounds) {
         },
         mark_dirty: function() {
-            var bounds = binding.GRect(0, 0, 144, 168);
+            var bounds = binding.GRect(0, 0, framebufferW, framebufferH);
             binding.graphics_context_set_fill_color(pebbleCtx, binding.GColorWhite);
             binding.graphics_fill_rect(pebbleCtx, bounds);
 
@@ -34,14 +41,22 @@ Rocky.bindCanvas= function(canvas) {
         },
         render_framebuffer: function() {
             // TODO: generalize this to render to other targets to enable better tooling
-            for (var y = 0; y < h; y++) {
-                for(var x = 0; x < w; x++) {
-                    var pebbleOffset = y * w + x;
+            for (var y = 0; y < canvasH; y++) {
+                var pebbleY = (y / scale) >> 0;
+                if (pebbleY >= framebufferH) {
+                    break;
+                }
+                for(var x = 0; x < canvasW; x++) {
+                    var pebbleX = (x / scale) >> 0;
+                    if (pebbleX >= framebufferW) {
+                        break;
+                    }
+                    var pebbleOffset = pebbleY * framebufferW + pebbleX;
                     var in_values = pebblePixels[pebbleOffset];
                     var r = ((in_values >> 4) & 0x3) * 85;
                     var g = ((in_values >> 2) & 0x3) * 85;
                     var b = ((in_values >> 0) & 0x3) * 85;
-                    var canvasOffset = pebbleOffset * 4;
+                    var canvasOffset = (y * canvasW + x) * 4;
                     canvasPixels[canvasOffset  ] = r;
                     canvasPixels[canvasOffset+1] = g;
                     canvasPixels[canvasOffset+2] = b;
