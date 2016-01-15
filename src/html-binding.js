457b28b546d65d887c25042ba396cbe1eb366aed
Heiko Behrens <mail@HeikoBehrens.net>
2016-01-14 18:25:58 -0800

    clean up src/html-bindings.js

--- src/html-binding.js
+++ src/html-binding.js
@@ -1,4 +1,11 @@
-/* Copyright © 2015-2016 Pebble Technology Corp., All Rights Reserved. http://pebble.github.io/rockyjs/LICENSE */
+/*
+
+    Copyright © 2015-2016 Pebble Technology Corp., All Rights Reserved. http://pebble.github.io/rockyjs/LICENSE
+
+    This describes functionality to create an instance of Rocky and bind it to an HTML canvas element.
+
+ */
+
 
 if (typeof(Rocky) == "undefined") {
     Rocky = {};
@@ -7,42 +14,51 @@ if (typeof(Rocky) == "undefined") {
 Rocky.bindCanvas= function(canvas, options) {
     options = options || {};
 
+    // instance of the Emscripten module
     var module = this.Module();
 
+    // in a future version, these values should adapt automatically
+    // also, we want the ability to create framebuffers of larger sizes
     var canvasW = canvas.width;
     var canvasH = canvas.height;
     var framebufferW = 144;
     var framebufferH = 168;
 
+    // scale gives us the ability to do a nearest-neighbor scaling
     var scale = options.scale || Math.min(canvasW/framebufferW, canvasH/framebufferH);
 
+    // pixel access to read (framebuffer) and write to (canvas)
     var canvasCtx = canvas.getContext("2d");
     var canvasPixelData = canvasCtx.createImageData(canvasW, canvasH);
     var canvasPixels = canvasPixelData.data;
-    var pebblePixelPTR = module.ccall("emx_graphics_get_pixels", "number", []);
-    var pebblePixels = new Uint8Array(module.HEAPU8.buffer, pebblePixelPTR, canvasW*canvasH);
-    var pebbleCtx = module.ccall("app_state_get_graphics_context", "number", []);
+    var framebufferPixelPTR = module.ccall("emx_graphics_get_pixels", "number", []);
+    var framebufferPixels = new Uint8Array(module.HEAPU8.buffer, framebufferPixelPTR, canvasW*canvasH);
+    var graphicsContext = module.ccall("app_state_get_graphics_context", "number", []);
 
-    var non_c_binding_keys = [];
+    // result of this function
     var binding = {
         module: module,
         update_proc: function(ctx, bounds) {
+            // meant to be override by clients
+            // will be called whenever a clients calls #mark_dirty()
         },
         mark_dirty: function() {
+            // initializes the graphics context and framebuffer with default values
+            // before rendering
             var bounds = binding.GRect(0, 0, framebufferW, framebufferH);
-            binding.graphics_context_set_fill_color(pebbleCtx, binding.GColorWhite);
-            binding.graphics_fill_rect(pebbleCtx, bounds);
+            binding.graphics_context_set_fill_color(graphicsContext, binding.GColorWhite);
+            binding.graphics_fill_rect(graphicsContext, bounds);
 
-            binding.graphics_context_set_fill_color(pebbleCtx, binding.GColorBlack);
-            binding.graphics_context_set_stroke_color(pebbleCtx, binding.GColorBlack);
-            binding.graphics_context_set_stroke_width(pebbleCtx, 1);
-            binding.graphics_context_set_antialiased(pebbleCtx, true);
+            binding.graphics_context_set_fill_color(graphicsContext, binding.GColorBlack);
+            binding.graphics_context_set_stroke_color(graphicsContext, binding.GColorBlack);
+            binding.graphics_context_set_stroke_width(graphicsContext, 1);
+            binding.graphics_context_set_antialiased(graphicsContext, true);
 
-            binding.update_proc(pebbleCtx, bounds);
+            binding.update_proc(graphicsContext, bounds);
             binding.render_framebuffer(canvasCtx);
         },
         render_framebuffer: function() {
-            // TODO: generalize this to render to other targets to enable better tooling
+            // renders current state of the framebuffer to the bound canvas respecting the passed scale
             for (var y = 0; y < canvasH; y++) {
                 var pebbleY = (y / scale) >> 0;
                 if (pebbleY >= framebufferH) {
@@ -54,7 +70,7 @@ Rocky.bindCanvas= function(canvas, options) {
                         break;
                     }
                     var pebbleOffset = pebbleY * framebufferW + pebbleX;
-                    var in_values = pebblePixels[pebbleOffset];
+                    var in_values = framebufferPixels[pebbleOffset];
                     var r = ((in_values >> 4) & 0x3) * 85;
                     var g = ((in_values >> 2) & 0x3) * 85;
                     var b = ((in_values >> 0) & 0x3) * 85;
@@ -68,6 +84,7 @@ Rocky.bindCanvas= function(canvas, options) {
             canvasCtx.putImageData(canvasPixelData, 0, 0);
         },
         export_global_c_symbols: function(global) {
+            // meant for simple scenarios where all C-like functions can live outside of this binding object
             if (typeof(global) == "undefined") {
                 global = window;
             }
@@ -82,17 +99,18 @@ Rocky.bindCanvas= function(canvas, options) {
     };
 
     // collect all functions of `binding` here so we can filter them out in export_global_c_symbols()
+    var non_c_binding_keys = [];
     for (var key in binding) {
         if (binding.hasOwnProperty(key)) {
             non_c_binding_keys.push(key);
         }
     }
 
+    // will enhance the binding object by various function from ./symbols-*.js of this folder
     Rocky.addGeneratedSymbols(binding);
     Rocky.addManualSymbols(binding);
 
-    // needed for Espruino compatibility (where functions are global and need a reference)
+    // useful if clients only have a single Rocky instance
     Rocky.activeBinding = binding;
     return binding;
 };
-
