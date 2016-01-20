94ad09cbe839e81eb7d68869169e3e6cfec8330e
Heiko Behrens <mail@HeikoBehrens.net>
2016-01-19 21:17:58 -0800

    simplify steps

--- tasks/dist.js
+++ tasks/dist.js
@@ -1,70 +1,32 @@
 module.exports = function(grunt) {
-    var Q = require("q");
-    var simpleGit = require("simple-git");
-    var fs = require('fs');
-    var _exec = Q.nfbind(require('child_process').exec);
 
-    var pkg = grunt.file.readJSON('package.json');
+    // location of our temporary working copy we use to build tags
     var tempCheckout = 'temp/checkout';
 
-    // promise wrapper around exec
-    var exec = function() {
-        grunt.verbose.writeln("exec:", arguments);
-        return _exec.apply(undefined, arguments);
-    };
+    // register a few promise related convenience functions
+    var exec = require('./promises')(grunt).exec;
+
+    var Q = require("q");
+    var fs = require('fs');
+    var simpleGit = require("simple-git");
 
     // rocky.js filename for a given version
     function rockyNameForTag(version) {
         return 'build/dist/rocky-' + version + '.js';
     }
 
-    // true, if a versioned rocky file exists in the destination filder
+    // true, if a versioned rocky file exists in the destination folder
     function versionedRockyExists(tag) {
         try {
-            var filename = rockyNameForTag(tag);
-            return fs.statSync(filename).isFile();
+            return fs.statSync(rockyNameForTag(tag)).isFile();
         } catch (err) {
             return false;
         }
     }
 
-    // promise wrapper around simpleGit + logging
-    function cloneAndReceiveRelevantTags(url, dir) {
-        var deferred = Q.defer();
-        grunt.log.write('cloning ' + url + ' into ' + dir + '...');
-        simpleGit('.').clone(url, 'temp/checkout').tags(function(err, tags) {
-            if (err) {
-                deferred.reject(err);
-                return;
-            }
-            grunt.log.ok("ok");
-
-            var foundTags = tags.all.filter(function(tag) {
-                return tag.match(/^v?\d\.\d\.\d$/);
-            });
-            grunt.log.writeln('found tags', foundTags);
-
-            var relevantTags = foundTags.filter(function(tag) {
-                return !versionedRockyExists(tag);
-            });
-            grunt.log.writeln('relevant tags', relevantTags);
-            deferred.resolve(relevantTags);
-        });
-        return deferred.promise;
-    }
-
-    // promise wrapper around simpleGit
-    function checkout(git, tag) {
-        var deferred = Q.defer();
-        git.checkout(tag, function(err) {
-            if (err) deferred.reject(err); else deferred.resolve();
-        });
-        return deferred.promise;
-    }
-
+    // creates a promise that expresses all steps for a given tag
     function checkoutNpmBuildCopy(tag) {
-        grunt.log.write("building and copying '" + tag + "'...");
-        return checkout(simpleGit(tempCheckout), tag)
+        return simpleGit(tempCheckout).promising('checkout', tag)
         .then(function() {
             return exec('npm install', {cwd: tempCheckout});
         })
@@ -72,15 +34,13 @@ module.exports = function(grunt) {
             return exec('grunt build', {cwd:tempCheckout});
         })
         .then(function() {
-            var taggedPkg = grunt.file.readJSON('package.json');
+            var taggedPkg = grunt.file.readJSON(tempCheckout + '/package.json');
             var destName = rockyNameForTag(taggedPkg.version);
             if (versionedRockyExists(taggedPkg.version)) {
-                grunt.log.warn("skipping file " + destName + ". Tag '" + tag + "' seems to be named incorrectly.");
+                grunt.log.warn("skipping file", destName, "Tag", tag, "seems to be named incorrectly.");
                 return false;
             }
-            return exec('cp ' + tempCheckout + '/' + destName + ' ' + destName).then(function() {
-                grunt.log.ok("ok");
-            });
+            return exec('cp ' + tempCheckout + '/' + destName + ' ' + destName);
         });
     }
 
@@ -88,12 +48,30 @@ module.exports = function(grunt) {
     var desc = 'Checks out and builds tags from remote repository and copies versioned output to ./build/dist';
     grunt.registerTask('build-missing-dists', desc, function() {
         var done = this.async();
+
+        // delete all of the temp folder
         exec( 'rm -rf temp')
+        // checkout Rocky.js repo into temp folder and retrieve tags
         .then(function() {
-            return cloneAndReceiveRelevantTags(pkg.repository.url, tempCheckout);
+            var url = grunt.file.readJSON('package.json').repository.url;
+            grunt.verbose.writeln('cloning', url, 'into', tempCheckout);
+            return simpleGit('.').clone(url, tempCheckout).promising('tags');
         })
+        // filter tags: keep version tags we don't have a rocky-x.y.z.js for, yet
+        .then(function(tags) {
+                var foundTags = tags.all.filter(function(tag) {
+                    return tag.match(/^v?\d\.\d\.\d$/);
+                });
+                grunt.verbose.writeln('found tags', foundTags);
+
+                var relevantTags = foundTags.filter(function(tag) {
+                    return !versionedRockyExists(tag);
+                });
+                grunt.log.writeln('relevant tags', relevantTags);
+                return relevantTags;
+        })
+        // create a sequence of promises to build all relevant tags
         .then(function(relevantTags) {
-            // this function returns a single promise that represents a chain of builds for the relevant tags
             var tasks = relevantTags.map(function(tag) {
                 return function(){return checkoutNpmBuildCopy(tag);};
             });
@@ -101,10 +79,12 @@ module.exports = function(grunt) {
                 return chain.then(t);
             }, Q(true));
         })
+        // grunt logging of any error case
         .catch(function (error) {
             grunt.log.error(error);
             throw error;
         })
+        // correctly terminate async grunt task
         .done(function(){done(true)}, function(){done(false)});
     });
 
---
+++ tasks/promises.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    var Q = require("q");
+    // add a few grunt-specific convenience functions to Promise
+    (function() {
+        var thenLogWithFunc = function (func) {
+            return function () {
+                var logArgs = arguments;
+                return this.then(function () {
+                    func.apply(undefined, logArgs);
+                    return arguments.length > 0 ? arguments[0] : undefined;
+                });
+            };
+        };
+        Q.makePromise.prototype.thenLog = thenLogWithFunc(grunt.log.writeln);
+        Q.makePromise.prototype.thenLogOk = thenLogWithFunc(function(){grunt.log.ok("OK")});
+        Q.makePromise.prototype.thenLogVerbose = thenLogWithFunc(grunt.verbose.writeln);
+    })();
+
+    // add support for promises to simpleGit
+    // use it by calling git.promising('checkout', '0.1.0')
+    require('simple-git/src/git').prototype.promising = function(fnName) {
+        var logMessage = ("git " + fnName + " " + Array.prototype.slice.call(arguments, 1).join()).trim() + "...";
+        grunt.log.write(logMessage);
+        var deferred = Q.defer();
+        var args = Array.prototype.slice.call(arguments, 1);
+        args.push(function(err, data) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(data);
+            }
+        });
+        this[fnName].apply(this, args);
+        return deferred.promise.thenLogOk();
+    };
+
+    // promise wrapper around exec + logging
+    var _exec = Q.nfbind(require('child_process').exec);
+    var exec = function(cmd, options) {
+        grunt.verbose.write("exec:", cmd, options ? options : {}, "...");
+        return _exec.apply(undefined, arguments).thenLogOk();
+    };
+
+    return {
+        exec: exec
+    };
+};
