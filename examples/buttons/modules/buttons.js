546d46ccc90debb75f3ca67d9b3cf127aa1c67f4
Katherine McAuliffe <kat@getpebble.com>
2016-02-17 15:05:47 -0800

    Heiko's comments

--- examples/buttons/modules/buttons.js
+++ examples/buttons/modules/buttons.js
@@ -1,23 +1,23 @@
 var buttonHandler = {};
 
+var buttonMap = {
+  back: 37,
+  up: 38,
+  select: 39,
+  down: 40
+};
+
 window.onPress = function onPress(type, callback) {
-  if (['back', 'up', 'select', 'down'].indexOf(type) >= 0) {
+  if (Object.keys(buttonMap).indexOf(type) >= 0) {
     buttonHandler[type] = callback;
   } else { console.log('Invalid type passed to onPress.');}
 };
 
 document.addEventListener('keydown', function(event) {
-  if (event.keyCode === 37 && buttonHandler.back) {
-    event.preventDefault();
-    buttonHandler['back']();
-  } else if (event.keyCode === 38 && buttonHandler.up) {
-    event.preventDefault();
-    buttonHandler['up']();
-  } else if (event.keyCode === 39 && buttonHandler.select) {
-    event.preventDefault();
-    buttonHandler['select']();
-  } else if (event.keyCode === 40 && buttonHandler.down) {
-    event.preventDefault();
-    buttonHandler['down']();
+  for (var button in buttonMap) {
+    if (event.keyCode === buttonMap[button] && buttonHandler[button]) {
+      event.preventDefault();
+      buttonHandler[button]();
+    }
   }
 });
