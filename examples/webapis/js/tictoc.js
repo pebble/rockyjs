987cee89be3c8fa437cc52b9d6e9486aebd1d5c5
Heiko Behrens <mail@HeikoBehrens.net>
2016-03-02 14:35:51 -0800

    some more cleanup

--- examples/webapis/js/tictoc.js
+++ examples/webapis/js/tictoc.js
@@ -7,7 +7,23 @@ var renderState = {
   hour: {style: 'red', scale: 0, angle: 0}
 };
 
-rocky.ondraw = function(drawEvent) {
+// helper function for the draw function (see below)
+// extracted as a standalone function to satisfy common believe in efficient JS code
+// TODO: verify that this has actually any effect on byte code level
+var drawHand = function(handState, ctx, cx, cy, maxRadius) {
+  ctx.lineWidth = 8;
+  ctx.strokeStyle = handState.style;
+  ctx.beginPath();
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx + Math.sin(handState.angle) * handState.scale * maxRadius,
+             cy + Math.cos(handState.angle) * handState.scale * maxRadius);
+  ctx.stroke();
+};
+
+// the 'draw' event is being emitted after each call to rocky.requestDraw() but
+// at most once for each screen update, even if .requestDraw() is called frequently
+// the 'draw' event might also fire at other meaningful times (e.g. upon launch)
+rocky.on('draw', function(drawEvent) {
   var ctx = drawEvent.context;
   var w = this.innerWidth;
   var h = this.innerHeight;
@@ -20,32 +36,26 @@ rocky.ondraw = function(drawEvent) {
   var cx = w / 2;
   var cy = h / 2;
   var maxRadius = Math.min(w, h) / 2;
+  drawHand(renderState.minute, ctx, cx, cy, maxRadius);
+  drawHand(renderState.hour, ctx, cx, cy, maxRadius);
+});
 
-  var drawHand = function(state) {
-    ctx.lineWidth = 8;
-    ctx.strokeStyle = state.style;
-    ctx.beginPath();
-    ctx.moveTo(cx, cy);
-    ctx.lineTo(cx + Math.sin(state.angle) * state.scale * maxRadius,
-               cy + Math.cos(state.angle) * state.scale * maxRadius);
-    ctx.stroke();
-  };
-
-  drawHand(renderState.minute);
-  drawHand(renderState.hour);
-};
-
+// animates the watchface by expanding the hands after we completely transitioned to the app.
+// we don't use .on('ready', f) as this would be too early for the desired visual effect.
 rocky.once('visibilitychange', function() {
+  // https://developer.mozilla.org/en-US/docs/Web/Events/visibilitychange
   // assumes this.visibilitystate === 'visible' on first and hence only call (.once)
 
   // micro implementation of TweenJS
-  this.tween(renderState, {eachStep: 'requestDraw'})
+  // http://www.createjs.com/docs/tweenjs/modules/TweenJS.html
+  this.tween(renderState, {onChange: rocky.requestDraw})
       .to({'minute.scale': 0.95,
              'hour.scale': 0.8}, 500, 'easeOutQuart');
 });
 
-// calls handler on each full minute and once immediately
+// listener is called on each full minute and once immediately after registration
 rocky.on('minutechange', function(e) {
+  // WatchfaceHelper will later be extracted as npm module
   var wfh = new Rocky.WatchfaceHelper(e.date);
   renderState.minute.angle = wfh.minuteAngle;
   renderState.hour.angle = wfh.hourAngle;
